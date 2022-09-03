a9644404dd6862e04c4d3a588836c0eb0093070b
LEEHI <myiron11@gmail.com>
2022-09-03 17:44:00 +0900

    solved2

--- cachematrix.R
+++ cachematrix.R
@@ -4,17 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
+  inv <- NULL ## 역행렬 저장변수 
+  set <- function(y) { 
     x <<- y
     inv <<- NULL
   }
-  get <- function() x
-  setinv <- function(solved) inv <<- solved
-  getinv <- function() inv
+  get <- function() x ## 저장된 행렬 가져옴
+  setinv <- function(solved) inv <<- solved ## 역행렬 설정
+  getinv <- function() inv ## 역행렬 반환
   list(set = set, get = get,
        setinv = setinv,
-       getinv = getinv)
+       getinv = getinv) ## list 출력
 }
 
 
@@ -23,12 +23,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   inv <- x$getinv()
-  if(!is.null(inv)){
+  if(!is.null(inv)){ ## 이미 캐시에 있다면
     message("getting cached solve")
     return(inv)
   }
-  data <- x$get()
-  inv <- solve(data, ...)
-  x$setinv(inv)
-  inv
+  data <- x$get() 
+  inv <- solve(data, ...) ## 역행렬 구하는 함수
+  x$setinv(inv) ## 설정
+  inv ## 반환
 }
